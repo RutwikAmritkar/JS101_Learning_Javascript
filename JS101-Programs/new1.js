a62a1c550244d908829cf23eb0c4ffa44caddaa3
RutwikAmritkar01 <rutwikamritkar007@gmail.com>
2023-04-13 21:44:37 +0530

    myfirstpush

---
+++ JS101-Programs/new1.js
@@ -0,0 +1,9 @@
+let i=10;
+let sum=0;
+let count=0;
+while(i<=10){
+  sum=sum+i
+  count=count+1
+  i++
+}
+console.log(sum/count)
---
+++ JS101-Programs/new6.js
@@ -0,0 +1,8 @@
+sum=0;
+for(i=1; i<=20; i++){
+  if((i%2==0) && (i%5==0)){
+    
+  }
+  sum=sum+i;
+}
+console.log(sum);
